f9843205ae9006f143318d415d4f98bed6faaa5a
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2022-06-22 19:13:29 -0300

    adicionado detalhes

--- scripts/detalhes.js
+++ scripts/detalhes.js
@@ -3,7 +3,18 @@ const id = urlParams.get('id');
 console.log(id)
 
 async function mostraDetalhes() {
-  const detalhes = Busca(`https://api.themoviedb.org/3/movie/${id}`).then(dados => console.log(dados))
+  const detalhes = await Busca(`https://api.themoviedb.org/3/movie/${id}`);
+  console.log(detalhes);
+  const poster = document.querySelector('#poster');
+  poster.src = "https://image.tmdb.org/t/p/w500" + detalhes.poster_path;
+  const titulo =  document.querySelector('#titulo');
+  titulo.innerHTML = detalhes.title;
+  const overview = document.querySelector('#overview');
+  overview.innerHTML = detalhes.overview;
+  let data = new Date(detalhes.release_date);
+  data = ((data.getDate() )) + "/" + ((data.getMonth() + 1)) + "/" + data.getFullYear();
+  const lancamento = document.querySelector('#lancamento');
+  lancamento.innerHTML += data;
 }
 
 mostraDetalhes()
